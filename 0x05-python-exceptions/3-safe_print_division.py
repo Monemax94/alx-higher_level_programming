62c43ce8046c2b647574195b5d22cad97e9580e6
Monemax94 <mondayodoh94gmail.com>
2022-09-12 18:55:31 +0100

    Mandatory Task3, a function that divides 2 integers and prints the result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+def safe_print_division(a, b):
+    try:
+        div = a // b 
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside Result: {}".format(div))
+        return (div)
