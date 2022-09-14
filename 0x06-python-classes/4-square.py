02d4180622ecc39d014b64c809770bdda1476c81
Monemax94 <mondayodoh94gmail.com>
2022-09-14 06:08:32 +0100

    Task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+"""Write a class Square that defines a square by: (based on 3-square.py) """
+
 class Square:
     """Represents a square.
     Private instance attribute: size:
