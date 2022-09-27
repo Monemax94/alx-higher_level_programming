0ae390e8d6ad725585b11355e15e11d2341f3323
Monemax94 <mondayodoh94gmail.com>
2022-09-27 20:31:56 +0100

    Updated

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-
 """
-File: 101-stats.py
-Desc: This module reads stdin line by line and computes metrics
-Author: Monday Odoh (Monemax)
-Date Created: Sep 27 2022
+Script reads stdin line by line and computes metrics
+Input format:
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+Each 10 lines and after a keyboard interruption (CTRL + C),
+prints those statistics since the beginning:
+total file size and
+possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+format: File size: <total size>
+format: <status code (in ascending order)>: <number>
 """
 
 
 def print_stats(size, status_codes):
-    """
-    This function prints the metrics
+    """Print accumulated metrics.
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
