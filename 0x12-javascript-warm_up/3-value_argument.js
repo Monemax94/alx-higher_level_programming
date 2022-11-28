116a22e952ba62d80bd0cee035703bcb856fabd3
Monemax94 <mondayodoh94gmail.com>
2022-11-28 17:16:49 +0100

    Task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 // This is a script that prints the first argument passed to it
 
-if (process.argv[2] === undifined) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
