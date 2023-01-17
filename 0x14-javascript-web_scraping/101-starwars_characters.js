0d3c4953a0883781eff5871a7b8a6ebf6e023f73
Monemax94 <mondayodoh94gmail.com>
2023-01-17 23:42:26 +0100

    Task101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,51 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-/*
-let characters = [];
-let dict = {};
-
-function addToDict (url, name) {
-  dict[url] = name;
-}
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  characters = JSON.parse(body).characters
-  characters.forEach(function (url) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      addToDict(url, JSON.parse(body).name);
-    });
-  });
-  characters.forEach(function (item) {
-    console.log(dict[item]);
-  })
+const args = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+request(url, async function (error, response, body) {
+  if (error === null) printer(JSON.parse(body).characters, 0);
 });
-*/
 
-function helpRequest (arr, i) {
-  if (i === arr.length) {
-    return;
-  }
-  request(arr[i], function (error, response, body) {
-    if (error) {
-      console.error(error);
+function printer (characters, i) {
+  request(characters[i], function (error, response, body) {
+    if (error === null) {
+      console.log(JSON.parse(response.body).name);
+      if (i + 1 < characters.length) printer(characters, i + 1);
     }
-    console.log(JSON.parse(body).name);
-    helpRequest(arr, i + 1);
   });
 }
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  const charac = JSON.parse(body).characters;
-  helpRequest(charac, 0);
-});
