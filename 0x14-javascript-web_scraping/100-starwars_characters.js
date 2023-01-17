50ea449d076f4b969f2d95ffd98eae6fd93285c8
Monemax94 <mondayodoh94gmail.com>
2023-01-17 23:39:14 +0100

    Task100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
+const args = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+request(url, function (error, response, body) {
+  if (error === null) {
+    const characters = JSON.parse(body).characters;
+    for (const idx in characters) {
+      request(characters[idx], function (error, response, body) {
+        if (error === null) console.log(JSON.parse(response.body).name);
+      });
+    }
   }
-  JSON.parse(body).characters.forEach(function (url, callback) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      console.log(JSON.parse(body).name);
-    });
-  });
 });
